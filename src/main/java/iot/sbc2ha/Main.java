17c6765aceb3e2b94ef592ce274c5f061d9e560b
Zbyszek Wieczorek <3379462+dafik@users.noreply.github.com>
2026-06-09 15:29:55 +0200

    chore(project): project skeleton (SBC-001)

---
+++ src/main/java/iot/sbc2ha/Main.java
@@ -0,0 +1,13 @@
+package iot.sbc2ha;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("sbc2ha starting...");
+        log.info("sbc2ha ready.");
+    }
+}
---
+++ src/test/java/iot/sbc2ha/MainTest.java
@@ -0,0 +1,12 @@
+package iot.sbc2ha;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    void testPlaceholder() {
+        assertTrue(true);
+    }
+}
